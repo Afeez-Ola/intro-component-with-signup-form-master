cb7d79589fe2eec3f6290dc3e2772306191825b2
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-17 12:34:54 +0100

    Modified the error message

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("form");
+const firstName = document.getElementById("fname")
+const lastName = document.getElementById("lname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const button = document.querySelector("button")
