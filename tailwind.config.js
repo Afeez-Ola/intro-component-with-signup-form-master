23c57430a1775e51a1b37fe46ea8cc24a95b869a
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-17 10:53:44 +0100

    structured

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          dark: "hsl(249, 10%, 26%) ",
+          light: "hsl(246, 25%, 77%)",
+          default: "hsl(248, 32%, 49%)"
+        },
+        red: "hsl(0, 100%, 74%) ",
+        green: "hsl(154, 59%, 51%)"
+
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
