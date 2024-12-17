6be3f972577b849faeed2e8370de737b57d722d7
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-12-17 12:36:43 +0100

    Going to shower now, will resume later :)

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const lastName = document.getElementById("lname");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const button = document.querySelector("button")
+
+
+function validateEmail(){
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+}
